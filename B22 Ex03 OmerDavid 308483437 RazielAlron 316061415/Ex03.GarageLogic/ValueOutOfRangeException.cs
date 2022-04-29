317b28810f3eda31373c7912695ba6842eb9addd
Omer David <omerdavid321@gmail.com>
2022-04-29 14:45:06 +0300

    WIP

--- B22 Ex03 OmerDavid 308483437 RazielAlron 316061415/Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ B22 Ex03 OmerDavid 308483437 RazielAlron 316061415/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -8,8 +8,8 @@ namespace Ex03.GarageLogic
 {
     class ValueOutOfRangeException : Exception
     {
-        public float m_MaxValue { get; set; }
-        public float m_MinValue { get; set; }
+        public float m_MaxValue { get; }
+        public float m_MinValue { get; }
 
         public ValueOutOfRangeException(float i_MaxValue, float i_MinValue)
         {
