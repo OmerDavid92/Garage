53db96207820db1cf3cdc4fff737853dfcdb3a92
Raziel Alron <razielron@gmail.com>
2022-04-29 18:33:41 +0300

    wip

---
+++ B22 Ex03 OmerDavid 308483437 RazielAlron 316061415/Ex03.GarageLogic/ElectricCar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    class ElectricCar
+    {
+        public enum CarColor
+        {
+            Red,
+            White,
+            Green,
+            Blue
+        }
+        
+        public CarColor m_CarColor { get; set; }
+        public float m_CargoVolume { get; }
+
+        public ElectricCar(CarColor i_CarColor, float i_CargoVolume)
+        {
+            m_CarColor = i_CarColor;
+            m_CargoVolume = i_CargoVolume;
+        }
+    }
+}
