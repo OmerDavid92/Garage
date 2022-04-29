57bce540791ab26ce973e5affdb584f853202d64
Omer David <omerdavid321@gmail.com>
2022-04-29 14:43:24 +0300

    Exceptions

---
+++ B22 Ex03 OmerDavid 308483437 RazielAlron 316061415/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    class ValueOutOfRangeException : Exception
+    {
+        public float m_MaxValue { get; set; }
+        public float m_MinValue { get; set; }
+
+        public ValueOutOfRangeException(float i_MaxValue, float i_MinValue)
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+        }
+    }
+}
